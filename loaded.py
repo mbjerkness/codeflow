c23c30999e4f90d9e8470340037cbb7d5c0d2b52
Paul Decoursey <paul@decoursey.net>
2014-11-05 21:47:21 -0600

    sublists

--- loaded.py
+++ loaded.py
@@ -1,7 +1,8 @@
 import yaml
-# from jinja2 import Environment
 from jinja2 import Template
 
+def isdict(thing):
+    return isinstance(thing, dict)
 
 with open("presentation.yml", 'r') as source:
     pres = yaml.load(source)
@@ -14,6 +15,7 @@ with open("presentation.yml", 'r') as source:
     with open("index-template.html", "r") as template_file:
         data = template_file.read()
         template = Template(data)
+        template.globals = {'isdict': isdict}
         rendered = template.render(**pres)
         with open("index.html", "w") as target:
             target.write(rendered)
